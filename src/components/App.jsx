a09cb29da97688e11a7038ce88d475e23b7e644d
om shankar <om8003golu@gmail.com>
2022-11-15 12:29:33 +0530

    file structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { CssBaseline } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
+
+import useStyles from './styles';
+
+import { Actors, MovieInformation, Movies, NavBar, Profile} from './';
 
 const App = () => {
+    const classes = useStyles();
   return (
-    <div>
-        App
+    <div className={classes.root}>
+        <CssBaseline />
+        <NavBar />
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
+            <Routes>
+                <Route path='/movie/:id' element={<MovieInformation />}></Route>
+                <Route path='/actors/:id' element={<Actors />}></Route>
+                <Route path='/' element={<Movies />}></Route>
+                <Route path='/profile/:id' element={<Profile />}></Route> 
+            </Routes>
+        </main>
     </div>
   );
 }
---
+++ src/components/Tlo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Tlo = () => {
+  return (
+    <div>Tlo</div>
+  );
+}
+
+export default Tlo;
